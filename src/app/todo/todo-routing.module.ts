6308433746d52386f760223fe6e770ecdc33a1f5
ynishimura0922 <ynishimura0922@gmail.com>
2019-11-05 01:02:59 +0900

    コンポーネントの初期設定

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TodoComponent } from './containers/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TodoRoutingModule } from './todo-routing.module';
+import { TodoComponent } from './containers/todo/todo.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TodoComponent],
   imports: [
     CommonModule,
     TodoRoutingModule
-  ]
+  ],
+  exports: [TodoComponent]
 })
 export class TodoModule { }
