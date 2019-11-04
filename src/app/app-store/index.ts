d4601b83b0f6b41f0665fa786e3f1060435b3653
ynishimura0922 <ynishimura0922@gmail.com>
2019-11-05 01:58:48 +0900

    Redux DevTools Extension用の処理追加

--- src/app/app-store/index.ts
+++ src/app/app-store/index.ts
@@ -1,20 +1,25 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from "@ngrx/store";
 import { environment } from '../../environments/environment';
 
+export interface State {}
 
-export interface State {
+export const reducers: ActionReducerMap<State> = {};
 
+/**
+ * Logger
+ */
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return (state, action) => {
+    const result = reducer(state, action);
+    console.groupCollapsed(action.type);
+    console.log('action', action);
+    console.log('state', result);
+    console.groupEnd();
+    return result;
+  };
 }
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+/**
+ * Meta reducer
+ */
+export const metaReducers: MetaReducer<State>[] = [logger];
