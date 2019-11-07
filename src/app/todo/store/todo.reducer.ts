bf32455affa51864a9e47cf1f2f98f63d816693c
ynishimura0922 <ynishimura0922@gmail.com>
2019-11-08 07:06:47 +0900

    初期状態とモデルを定義

--- src/app/todo/store/todo.reducer.ts
+++ src/app/todo/store/todo.reducer.ts
@@ -1,14 +1,17 @@
 import { Action, createReducer, on } from '@ngrx/store';
-
+import { Todo } from '../models/todo.model';
 
 export const todoFeatureKey = 'todo';
 
 export interface State {
-
+  loading: boolean;
+  todos: Todo[];
+  error?: any;
 }
 
 export const initialState: State = {
-
+  loading: false,
+  todos: []
 };
 
 const todoReducer = createReducer(
